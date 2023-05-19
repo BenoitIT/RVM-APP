a353eafed17b05c97cedc5ec853fb4b225560637
benoitIT <benoitnda35@gmail.com>
2023-05-19 22:38:32 +0200

    setup stack navigator

--- App.js
+++ App.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { SafeAreaView, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import SignUp from './components/pages/signUp';
 import  Home  from './components/pages/Home';
 import Login from './components/pages/Login';
 export default function App() {
+
+const Stack = createNativeStackNavigator();
   return (
-    <SafeAreaView className="flex-1 items-center justify-center item-center">
-    <View>
-     <SignUp/>
-    </View>
+    <SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
     </SafeAreaView>
   );
 }
